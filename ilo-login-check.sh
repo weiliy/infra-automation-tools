5472057eee036ba3ab4e9a141c374333a9fc20f6
weiliy <weiliy@users.noreply.github.com>
2014-11-07 18:24:36 +0800

    v0.1 released

--- ilo-login-check.sh
+++ ilo-login-check.sh
@@ -6,7 +6,7 @@
 # 2014/11/07	Weili	First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-
+#set -x
 
 while getopts :u:p: arg
 do
@@ -22,14 +22,15 @@ done
 shift $(($OPTIND - 1))
 ilolist=("$@")
 
-echo username=$ilousername
-echo password=$ilopassword
-echo serverlist:
-echo ${ilolist[@]}
+#echo username=$ilousername
+#echo password=$ilopassword
+#echo serverlist:
+#echo ${ilolist[*]}
 
-export isLogin=0
-checkLogin()
-{
+echo "* $0"
+
+for ip in ${ilolist[@]}
+do
 	isLogin=`expect -c "
 			spawn ssh $ip -l $ilousername
 			expect {
@@ -38,18 +39,21 @@ checkLogin()
 				> {send \"exit\r\"; exp_continue}
 			}
 		" | grep "logged" | wc -l`
-}
-for ip in ${ilolist[@]}
-do
-	echo Trying to connect to $ip...
-	checkLogin()
 	case $isLogin in
-		0)	echo "Login to $ip failed."
-			failedIPs=("$failedIPs[*]" $ip)
+		0)	echo "* Login to $ip failed."
+			failedIPs=($(echo "${failedIPs[*]} $ip"))
 			;;
-		1)	echo "Login to $ip successfully."
-			succIPs=("$succIPs[*]" $ip)
+		1)	echo "* Login to $ip successfully."
+			succIPs=($(echo "${succIPs[*]} $ip"))
 			;;
-	esac
 	esac
 done
+
+echo "-------------------------------------"
+echo "Summary"
+echo "-------------------------------------"
+echo "Successful login: (total ${#succIPs[*]})"
+echo ${succIPs[*]}
+echo "Failed login:     (total ${#failedIPs[*]})"
+echo ${failedIPs[*]}
+
